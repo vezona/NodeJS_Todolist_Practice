32a7543441b58792c012da7473feb64ed9db0e63
vezonaChin <vezona5tv@gmail.com>
2023-04-14 18:45:36 +0800

    schema、Model 寫資料進資料庫

--- mongooseServer.js
+++ mongooseServer.js
@@ -6,11 +6,54 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017/hotel')
     .then((res) => {
         console.log('資料庫連線成功');
-      
     })
     .catch(err => {
       console.log(err);
     })
+
+// 設定 schema
+const roomSchema = {
+    name: String,
+    price: {
+        type: Number,
+        required: true, // 也可以寫這樣 [true, "價格必填"]，字串帶自己想傳的字
+    },
+    rating:Number,
+}
+
+const versionKey = new mongoose.Schema(
+    {  // 第一個參數，放要新增的欄位型別
+        name: String,
+        price: {
+            type: Number,
+            required: true, 
+        },
+        rating:Number,
+    },
+    {  // 第二個參數，放預設欄位
+        versionKey: false
+    }
+)
+
+// 設定 Model 
+const Room = mongoose.model('Room', roomSchema)
+
+// 建立實例/實體
+const testRoom = new Room({
+    name: '總統套房',
+    price:2000,
+    rating:4.5
+})
+
+// 存進資料庫
+testRoom.save()
+    .then(() => {
+        console.log('成功');
+    }).catch(err => {
+        console.log(err);
+    })
+
+
   
 // 設定監聽
 const requestListener = (req,res)=>{
